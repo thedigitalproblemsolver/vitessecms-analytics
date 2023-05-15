255eaf04fe63dcd77e42995135e540457930c6b0
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-05-15 23:21:48 +0200

    Added Editable Added Deletable

--- src/Controllers/AdminanalyticsentryController.php
+++ src/Controllers/AdminanalyticsentryController.php
@@ -3,29 +3,65 @@
 namespace VitesseCms\Analytics\Controllers;
 
 use stdClass;
+use VitesseCms\Admin\Interfaces\AdminModelDeletableInterface;
+use VitesseCms\Admin\Interfaces\AdminModelEditableInterface;
 use VitesseCms\Admin\Interfaces\AdminModelListInterface;
+use VitesseCms\Admin\Traits\TraitAdminModelDeletable;
 use VitesseCms\Admin\Traits\TraitAdminModelList;
 use VitesseCms\Analytics\Enums\AnalyticsEntryEnum;
 use VitesseCms\Analytics\Models\AnalyticsEntryIterator;
 use VitesseCms\Analytics\Repositories\AnalyticsEntryRepository;
 use VitesseCms\Core\AbstractControllerAdmin;
+use VitesseCms\Database\AbstractCollection;
+use VitesseCms\Language\Enums\LanguageEnum;
+use VitesseCms\Language\Services\LanguageService;
 
-class AdminanalyticsentryController extends AbstractControllerAdmin implements AdminModelListInterface
+class AdminanalyticsentryController extends AbstractControllerAdmin implements
+    AdminModelListInterface,
+    AdminModelDeletableInterface,
+    AdminModelEditableInterface
 {
     use TraitAdminModelList;
+    use TraitAdminModelDeletable;
 
-    private AnalyticsEntryRepository $analyticsEntryRepository;
+    private readonly AnalyticsEntryRepository $analyticsEntryRepository;
+    private readonly LanguageService $languageService;
 
     public function OnConstruct()
     {
         parent::OnConstruct();
 
         $this->analyticsEntryRepository = $this->eventsManager->fire(AnalyticsEntryEnum::GET_REPOSITORY->value, new stdClass());
-
+        $this->languageService = $this->eventsManager->fire(LanguageEnum::ATTACH_SERVICE_LISTENER->value, new stdClass());
     }
 
     public function getModelList(): AnalyticsEntryIterator
     {
         return $this->analyticsEntryRepository->findAll(false, 10);
+    }
+
+    public function isDeletable(): bool
+    {
+        return true;
+    }
+
+    public function isEditable(): bool
+    {
+        return true;
+    }
+
+    public function editAction(string $id): void
+    {
+        // TODO: Implement editAction() method.
+    }
+
+    public function getDeletableModel(string $id): ?AbstractCollection
+    {
+        return $this->analyticsEntryRepository->getById($id, false);
+    }
+
+    public function getEditableModel(string $id): ?AbstractCollection
+    {
+        return $this->analyticsEntryRepository->getById($id, false);
     }
 }
