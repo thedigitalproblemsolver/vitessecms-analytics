1d1602f78fa7416131d270f35e2993aafb471e14
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-12-16 10:45:46 +0100

    Added latestClick block

---
+++ src/Blocks/LatestClicks.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VitesseCms\Analytics\Blocks;
+
+use Phalcon\Di\Di;
+use stdClass;
+use VitesseCms\Analytics\Enums\ClickEntryEnum;
+use VitesseCms\Analytics\Repositories\ClickEntryRepository;
+use VitesseCms\Block\AbstractBlockModel;
+use VitesseCms\Block\Models\Block;
+use VitesseCms\Content\Enum\ItemEnum;
+use VitesseCms\Content\Repositories\ItemRepository;
+use VitesseCms\Core\Services\ViewService;
+use VitesseCms\Database\Models\FindOrder;
+use VitesseCms\Database\Models\FindOrderIterator;
+use VitesseCms\Database\Models\FindValue;
+use VitesseCms\Database\Models\FindValueIterator;
+
+class LatestClicks extends AbstractBlockModel
+{
+    private ClickEntryRepository $clickEntryRepository;
+    private ItemRepository $itemRepository;
+
+    public function __construct(ViewService $view, Di $di)
+    {
+        parent::__construct($view, $di);
+
+        $this->clickEntryRepository = $di->get('eventsManager')->fire(
+            ClickEntryEnum::GET_REPOSITORY->value,
+            new stdClass()
+        );
+        $this->itemRepository = $di->get('eventsManager')->fire(ItemEnum::GET_REPOSITORY, new stdClass());
+    }
+
+    public function getTemplateParams(Block $block): array
+    {
+        $params = parent::getTemplateParams($block);
+
+        $clickEntries = $this->clickEntryRepository->findAll(
+            null,
+            true,
+            30,
+            new FindOrderIterator([new FindOrder('createdAt', -1)])
+        );
+
+        $items = [];
+        $parsed = [];
+        while ($clickEntries->valid()) {
+            $clickEntry = $clickEntries->current();
+            if (!isset($parsed[$clickEntry->slug]) && count($items) < 12) {
+                $items[] = $this->itemRepository->findFirst(
+                    new FindValueIterator(
+                        [
+                            new FindValue(
+                                'slug.' . $this->di->get('configuration')->getLanguageShort(),
+                                ltrim($clickEntry->slug, '/')
+                            )
+                        ]
+                    )
+                );
+                $parsed[$clickEntry->slug] = '';
+            }
+
+            $clickEntries->next();
+        }
+
+        $params['items'] = $items;
+
+        return $params;
+    }
+}
