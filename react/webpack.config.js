438291969f6f97ccadde8e15edb363ea3793e31d
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-02-20 12:01:55 +0100

    Moved React code outside of PHP root

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.tsx',
-  output: { path: path.resolve(__dirname, '../js') },
+  output: { path: path.resolve(__dirname, '../src/Resources/js') },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
