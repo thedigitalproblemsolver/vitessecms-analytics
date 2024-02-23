86bb37e97202c65068f47f52abd7b321f0c7edec
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-02-23 23:01:43 +0100

    Added dynamically return on AdmindashboardController.php

--- src/Controllers/AdmindashboardController.php
+++ src/Controllers/AdmindashboardController.php
@@ -4,10 +4,40 @@ declare(strict_types=1);
 
 namespace VitesseCms\Analytics\Controllers;
 
+use VitesseCms\Analytics\Models\AnalyticsEntry;
+use VitesseCms\Analytics\Models\ClickEntry;
+use VitesseCms\Analytics\Models\WebCrawlerEntry;
+use VitesseCms\Analytics\Repositories\AnalyticsEntryRepository;
+use VitesseCms\Analytics\Repositories\ClickEntryRepository;
+use VitesseCms\Analytics\Repositories\WebCrawlerEntryRepository;
 use VitesseCms\Core\AbstractControllerAdmin;
+use VitesseCms\Database\DTO\GetRepositoryDTO;
+use VitesseCms\Database\Enums\RepositoryEnum;
 
 final class AdmindashboardController extends AbstractControllerAdmin
 {
+    private AnalyticsEntryRepository $analyticsEntryRepository;
+    private ClickEntryRepository $clickEntryRepository;
+    private WebCrawlerEntryRepository $webCrawlerEntryRepository;
+
+    public function OnConstruct()
+    {
+        parent::OnConstruct();
+
+        $this->analyticsEntryRepository = $this->eventsManager->fire(
+            RepositoryEnum::GET_REPOSITORY->value,
+            new GetRepositoryDTO(AnalyticsEntry::class)
+        );
+        $this->clickEntryRepository = $this->eventsManager->fire(
+            RepositoryEnum::GET_REPOSITORY->value,
+            new GetRepositoryDTO(ClickEntry::class)
+        );
+        $this->webCrawlerEntryRepository = $this->eventsManager->fire(
+            RepositoryEnum::GET_REPOSITORY->value,
+            new GetRepositoryDTO(WebCrawlerEntry::class)
+        );
+    }
+
     public function indexAction(): void
     {
         $mainJs = file_get_contents(__DIR__.'/../Resources/js/main.js');
@@ -21,60 +51,21 @@ final class AdmindashboardController extends AbstractControllerAdmin
     public function getPageViewsByDayAction(): void
     {
         $this->jsonResponse([
-            'data' => [
-                [
-                    'date' => '2024-02-16',
-                    'amount' => rand(0, 30),
-                ],
-                [
-                    'date' => '2024-02-15',
-                    'amount' => rand(0, 30),
-                ],
-                [
-                    'date' => '2024-02-14',
-                    'amount' => rand(0, 30),
-                ],
-            ],
+            'data' => array_reverse($this->analyticsEntryRepository->getPageViewsByDay()->toArray()),
         ]);
     }
 
     public function getClicksByDayAction(): void
     {
         $this->jsonResponse([
-            'data' => [
-                [
-                    'date' => '2024-02-16',
-                    'amount' => rand(0, 999),
-                ],
-                [
-                    'date' => '2024-02-15',
-                    'amount' => rand(0, 999),
-                ],
-                [
-                    'date' => '2024-02-14',
-                    'amount' => rand(0, 999),
-                ],
-            ],
+            'data' => array_reverse($this->clickEntryRepository->getClicksByDay()->toArray()),
         ]);
     }
 
     public function getWebcrawlerVisitsByDayAction(): void
     {
         $this->jsonResponse([
-            'data' => [
-                [
-                    'date' => '2024-02-16',
-                    'amount' => rand(0, 999),
-                ],
-                [
-                    'date' => '2024-02-15',
-                    'amount' => rand(0, 999),
-                ],
-                [
-                    'date' => '2024-02-14',
-                    'amount' => rand(0, 999),
-                ],
-            ],
+            'data' => array_reverse($this->webCrawlerEntryRepository->getWebcrawlerVisitsByDay()->toArray()),
         ]);
     }
 }
